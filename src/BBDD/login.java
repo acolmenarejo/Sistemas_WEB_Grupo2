9da80631671a4997b7c41e1d87739bec33a41922
Carlos Palomo Rey <c.palomo1@usp.ceu.es>
2020-12-30 20:37:40 +0100

    Login correcto, falta redirigir

--- src/BBDD/login.java
+++ src/BBDD/login.java
@@ -1,22 +1,28 @@
 package BBDD;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.swing.JOptionPane;
 
-@WebServlet(name="registro", urlPatterns= {"/registro"})
-public class registro extends HttpServlet{
-
+/**
+ * Servlet implementation class login
+ */
+@WebServlet(name="login", urlPatterns= {"/login"})
+public class login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
 	private Connection connection = null;
 	private Statement statement = null;
 	ResultSet resultSet = null;
@@ -56,83 +62,57 @@ public class registro extends HttpServlet{
 	}
 	
 	
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String correo = req.getParameter("email");
-		String contrasena = req.getParameter("contrasena");
-		System.out.println(contrasena);
-		//String query = "select * from usuario";;
-		String mensaje = "";
-		try {
-			//edad = Integer.parseInt(req.getParameter("edad"));
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-		try {
-		String query = "insert into usuario values( NULL,'A', '" + contrasena + "', '" + correo + "')";
-		
-		/*
-		
-			System.out.println("previo0");
-			synchronized(statement) {
-				System.out.println("previo1");
-				resultSet = statement.executeQuery(query);
-				System.out.println("previo2");
-			}
-			while(resultSet.next()) {
-				System.out.println("previo4");
-				if(query!="NULL" || resultSet.getString("correo").equals(correo)) {
-					System.out.println("correo existente");
-				}else {
-					System.out.println("connected2");
-					query = "insert into usuario values( NULL,'A', '" + contraseña + "', '" + correo + "')";
-				}
-										
-				}
-			*/
-			synchronized(statement) {
-				statement.executeUpdate(query);
-			}
-			
-			
-			mensaje = "BD ha actualizado correctamente";
-		} catch(Exception e) {
-			mensaje = "Error al insertar datos";
-			System.out.println(e);
-		}
-		
-		resp.setContentType("text/html;charset=UTF-8");
-		try(PrintWriter out = resp.getWriter()){
-			out.println("<html>");
-			out.println("<head><title>Registro</title></head>");
-			out.println("<body>");
-			out.println("<h1>" + mensaje + "</h1>");
-			out.println("</body></html>");
-		}
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	/*
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
-	
-
-	
-	@Override
-	public void destroy() {
-		try {
-			statement.close();
-		} catch (Exception e) {
-			
-		} finally {
+	*/
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String email = req.getParameter("email");
+		String contrasena = req.getParameter("contrasena");
+		ServletContext context = req.getServletContext();
+		
+		String query = "SELECT 1 FROM usuario WHERE correo='" + email + "' AND contrasena='" + contrasena + "'";
+		synchronized(statement) {
 			try {
-				connection.close();
+				resultSet = statement.executeQuery(query);
+				System.out.println(resultSet);
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
+			if(resultSet.equals(null)){
+				JOptionPane.showMessageDialog(null, "No existe el usuario");
+				System.out.println("No existe el usuario");
+			} else {
+				JOptionPane.showMessageDialog(null, "Todo OK todo GUCCI");
+				System.out.println("Todo OK todo GUCCI");
+			}
 	}
+
 	
-	@Override
-	public String getServletInfo() {
-		return "Contador Cookies";
-	}	
+	/*
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		//String password = req.getParameter("password");
+		ServletContext context = req.getServletContext();
+		if(password.equals(passwordAdmin)) {
+			RequestDispatcher addPuntuacion = context.getNamedDispatcher("AddPuntuacion");
+			req.setAttribute("autenticado", true);
+			addPuntuacion.forward(req, resp);
+		} else {
+			RequestDispatcher error = context.getRequestDispatcher("/ejemplos06/error.html");
+			error.forward(req, resp);
+		}
+	}
+	*/
 	
 	
 }
