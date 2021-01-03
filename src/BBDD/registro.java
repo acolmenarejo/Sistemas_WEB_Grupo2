dd8160abbdc0128dd4871686203b49b51808dfd5
elisacoello <32563382+elisacoello@users.noreply.github.com>
2021-01-03 12:06:14 +0100

    Campos añadidos en la tabla de post

--- src/BBDD/registro.java
+++ src/BBDD/registro.java
@@ -42,6 +42,8 @@ public class registro extends HttpServlet{
 			statement.execute("CREATE TABLE IF NOT EXISTS post (\r\n"
 					+ "    id_post INT NOT NULL AUTO_INCREMENT,\r\n"
 					+ "    id INT NOT NULL,\r\n"
+					+ "    titulo TEXT NOT NULL,\r\n"
+					+ "    tematica TEXT NOT NULL,\r\n"
 					+ "    contenido TEXT NOT NULL,\r\n"
 					+ "    PRIMARY KEY(id_post),\r\n"
 					+ "    INDEX(id),\r\n"
